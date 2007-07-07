f6d85c33d235c8c7380b1c8c503dc688e3dced02
Aldo Cortesi <aldo@nullcube.com>
2007-07-07 11:47:15 +1000

    Create a MANIFEST.in, and modify setup.py, .gitignore, etc. appropriately.

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from distutils.core import setup
 
 setup(  name="cubictemp",
         version="2.0",
-        description="A Python templating module",
+        description="A more elegant templating module from a more civilised age",
         author="Nullcube Pty Ltd",
         author_email="aldo@nullcube.com",
+        license="MIT",
         url="http://www.nullcube.com",
         py_modules = ["cubictemp"]
     )
