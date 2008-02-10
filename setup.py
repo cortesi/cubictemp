65a338a720b7b3d1d8f3eff1ad8269d5b7205ae5
Aldo Cortesi <aldo@nullcube.com>
2008-02-10 19:59:39 +1100

    Begin to prep Cubictemp for a release.
        - Fix doc rendering under new Countershape.
        - Add classifiers to setup.py
        - Remove Xara image source file, and add Inkscape SVG.
        - Add a README to the test suite pointing to pry

--- setup.py
+++ setup.py
@@ -3,10 +3,18 @@ from distutils.core import setup
 setup(
         name="cubictemp",
         version="2.0-alpha",
-        description="A more elegant templating module from a more civilised age",
+        description="A more elegant templating library for a more civilised age",
         author="Nullcube Pty Ltd",
         author_email="aldo@nullcube.com",
         license="MIT",
-        url="http://www.nullcube.com",
-        py_modules = ["cubictemp"]
-    )
+        url="http://dev.nullcube.com",
+        py_modules = ["cubictemp"],
+        classifiers = [
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python",
+            "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+            "Topic :: Software Development :: Libraries",
+            "Topic :: Text Processing"
+        ]
+)
