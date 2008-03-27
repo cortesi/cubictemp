a4d7dfe311ff0bc63dc4f4ceada7b4c4e1267af2
Aldo Cortesi <aldo@nullcube.com>
2008-03-27 13:40:04 +1100

    - Refine trove specifiers - Remove redundant image in docs

--- setup.py
+++ setup.py
@@ -2,17 +2,20 @@ from distutils.core import setup
 
 setup(
         name="cubictemp",
-        version="2.0-alpha",
+        version="2.0",
         description="A more elegant templating library for a more civilised age",
         author="Nullcube Pty Ltd",
         author_email="aldo@nullcube.com",
         license="MIT",
         url="http://dev.nullcube.com",
+        download_url="http://dev.nullcube.com/download/cubictemp2.0.tar.gz",
         py_modules = ["cubictemp"],
         classifiers = [
             "Intended Audience :: Developers",
             "License :: OSI Approved :: MIT License",
+            "Development Status :: 5 - Production/Stable",
             "Programming Language :: Python",
+            "Operating System :: OS Independent",
             "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
             "Topic :: Software Development :: Libraries",
             "Topic :: Text Processing"
